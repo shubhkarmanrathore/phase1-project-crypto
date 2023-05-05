399f6d71baef481cb51d0a0aac0ed5326976f6f1
Shubhkarman Singh Rathore <shubhkarman4733@yahoo.in>
2023-05-05 16:44:24 -0700

    adds coin data

--- index.js
+++ index.js
@@ -12,3 +12,37 @@ async function getCoins() {
     console.error(error);
   }
 }
+
+// Display coin data in the grid
+function displayCoins(coins) {
+    coinsGrid.textContent = '';
+    coins.slice(0, 20).forEach(coin => {
+      const { id, rank, symbol, name, priceUsd, changePercent24Hr } = coin;
+  
+      const card = document.createElement('div');
+      card.classList.add('coin-card');
+      card.addEventListener('click', () => {
+        window.location.href = `https://coinmarketcap.com/currencies/${id}`;
+      });
+  
+      const image = document.createElement('img');
+      image.src = `https://crypto.com/price/_next/image?url=https%3A%2F%2Fstatic.crypto.com%2Ftoken%2Ficons%2F${id}%2Fcolor_icon.png&w=64&q=75`;
+      image.alt = `${name} icon`;
+  
+      const heading = document.createElement('h2');
+      heading.textContent = `${name} (${symbol})`;
+  
+      const price = document.createElement('p');
+      price.textContent = `Price: $${parseFloat(priceUsd).toFixed(2)}`;
+  
+      const change = document.createElement('p');
+      change.textContent = `24h Change: ${parseFloat(changePercent24Hr).toFixed(2)}%`;
+  
+      card.appendChild(image);
+      card.appendChild(heading);
+      card.appendChild(price);
+      card.appendChild(change);
+      coinsGrid.appendChild(card);
+      
+    });
+  }
