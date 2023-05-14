c205e07f49cf3b362097fa510616ad84aac4e6eb
Shubhkarman Singh Rathore <shubhkarman4733@yahoo.in>
2023-05-13 19:53:22 -0700

    formatted code

--- index.js
+++ index.js
@@ -1,74 +1,70 @@
 document.addEventListener('DOMContentLoaded', () => {
-const Url = 'https://api.coincap.io/v2/assets';
-const coinsGrid = document.querySelector('.coins-grid');
-const searchInput = document.getElementById('search-input');
+  const Url = 'https://api.coincap.io/v2/assets';
+  const coinsGrid = document.querySelector('.coins-grid');
+  const searchInput = document.getElementById('search-input');
 
-
-// Fetch data from CoinCap API
-async function getCoins() {
-  try {
-    const response = await fetch(Url);
-    const data = await response.json();
-    return data.data;
-  } catch (error) {
-    console.error(error);
+  // Fetch data from CoinCap API
+  async function getCoins() {
+    try {
+      const response = await fetch(Url);
+      const data = await response.json();
+      return data.data;
+    } catch (error) {
+      console.error(error);
+    }
   }
-}
 
-
-// Display coin data in the grid
-function displayCoins(coins) {
+  // Display coin data in the grid
+  function displayCoins(coins) {
     coinsGrid.textContent = '';
     coins.slice(0, 20).forEach(coin => {
-        const { id, rank, symbol, name, supply, maxSupply, marketCapUsd, volumeUsd24Hr,  priceUsd, changePercent24Hr, vwap24Hr } = coin;
-  
-    const card = document.createElement('div');
-    card.classList.add('coin-card');
-  
-    const image = document.createElement('img');
-    image.src = `https://crypto.com/price/_next/image?url=https%3A%2F%2Fstatic.crypto.com%2Ftoken%2Ficons%2F${id}%2Fcolor_icon.png&w=64&q=75`;
-    image.alt = `${name} icon`;
-  
-    const heading = document.createElement('h2');
-    heading.textContent = `${name} (${symbol})`;
-  
-    const price = document.createElement('p');
-    price.textContent = `Price: $${parseFloat(priceUsd).toFixed(2)}`;
-  
-    const change = document.createElement('p');
-    change.textContent = `24h Change: ${parseFloat(changePercent24Hr).toFixed(2)}%`;
-    const arrow = document.createElement('span');
-    arrow.classList.add('arrow');
+      const { id, rank, symbol, name, supply, maxSupply, marketCapUsd, volumeUsd24Hr,  priceUsd, changePercent24Hr, vwap24Hr } = coin;
 
-    if (changePercent24Hr >= 0) {
-      arrow.textContent = '   ⇧';
-      arrow.classList.add('green-arrow');
-    } else {
-      arrow.textContent = '   ⇩';
-      arrow.classList.add('red-arrow');
-    }
+      const card = document.createElement('div');
+      card.classList.add('coin-card');
 
-    const dailyVolume = document.createElement('p');
-    dailyVolume.textContent = `24hr Volume: $${(parseFloat(volumeUsd24Hr) / 1000000000).toFixed(2)}B`;
+      const image = document.createElement('img');
+      image.src = `https://crypto.com/price/_next/image?url=https%3A%2F%2Fstatic.crypto.com%2Ftoken%2Ficons%2F${id}%2Fcolor_icon.png&w=64&q=75`;
+      image.alt = `${name} icon`;
 
-    const marketCap = document.createElement('p');
-    marketCap.textContent = `Market Cap: $${(parseFloat(marketCapUsd) / 1000000000).toFixed(2)}B`;
+      const heading = document.createElement('h2');
+      heading.textContent = `${name} (${symbol})`;
 
+      const price = document.createElement('p');
+      price.textContent = `Price: $${parseFloat(priceUsd).toFixed(2)}`;
 
-    change.appendChild(arrow);
-    card.appendChild(image);
-    card.appendChild(heading);
-    card.appendChild(price);
-    card.appendChild(change);
-    card.appendChild(dailyVolume)
-    card.appendChild(marketCap);
-    coinsGrid.appendChild(card);
+      const change = document.createElement('p');
+      change.textContent = `24h Change: ${parseFloat(changePercent24Hr).toFixed(2)}%`;
+      const arrow = document.createElement('span');
+      arrow.classList.add('arrow');
+
+      if (changePercent24Hr >= 0) {
+        arrow.textContent = '   ⇧';
+        arrow.classList.add('green-arrow');
+      } else {
+        arrow.textContent = '   ⇩';
+        arrow.classList.add('red-arrow');
+      }
+
+      const dailyVolume = document.createElement('p');
+      dailyVolume.textContent = `24hr Volume: $${(parseFloat(volumeUsd24Hr) / 1000000000).toFixed(2)}B`;
+
+      const marketCap = document.createElement('p');
+      marketCap.textContent = `Market Cap: $${(parseFloat(marketCapUsd) / 1000000000).toFixed(2)}B`;
+
+      change.appendChild(arrow);
+      card.appendChild(image);
+      card.appendChild(heading);
+      card.appendChild(price);
+      card.appendChild(change);
+      card.appendChild(dailyVolume)
+      card.appendChild(marketCap);
+      coinsGrid.appendChild(card);
     });
   }
 
-
-// Search for coins
-function searchCoins(event) {
+  // Search for coins
+  function searchCoins(event) {
     const searchValue = event.target.value.toLowerCase();
     getCoins().then(coins => {
       const filteredCoins = coins.filter(coin => {
@@ -78,48 +74,41 @@ function searchCoins(event) {
       displayCoins(filteredCoins);
     });
   }
-  
 
-// Load and display the top 20 coins on page load
-getCoins().then(coins => {
+  // Load and display the top 20 coins on page load
+  getCoins().then(coins => {
     displayCoins(coins);
-});
-  
+  });
 
-// Add event listener to search input
-searchInput.addEventListener('input', searchCoins);
-  
+  // Add event listener to search input
+  searchInput.addEventListener('input', searchCoins);
 
-//Refresh page from logo and home button
-const h1 = document.querySelector('h1')
-const homeButton= document.querySelector('#homeButton')
-h1.addEventListener('click', reloadPage)
-homeButton.addEventListener('click', reloadPage)
-function reloadPage() {
+  //Refresh page from logo and home button
+  const h1 = document.querySelector('h1')
+  const homeButton= document.querySelector('#homeButton')
+  h1.addEventListener('click', reloadPage)
+  homeButton.addEventListener('click', reloadPage)
+  function reloadPage() {
     location.reload()
-}
+  }
 
+  // Dark Mode toggle
+  const toggle = document.querySelector('.toggle');
+  const toggleLabel = document.querySelector('.toggle-label');
+  const body = document.querySelector('body');
 
+  toggle.addEventListener('change', function() {
+    if (this.checked) {
+      body.classList.add('dark-mode');
+      toggleLabel.classList.add('toggle-label--checked');
+    } else {
+      body.classList.remove('dark-mode');
+      toggleLabel.classList.remove('toggle-label--checked');
+    }  
+  });
 
-
-//Dark Mode toggle
-const toggle = document.querySelector('.toggle');
-const toggleLabel = document.querySelector('.toggle-label');
-const body = document.querySelector('body');
-
-toggle.addEventListener('change', function() {
-  if (this.checked) {
-    body.classList.add('dark-mode');
-    toggleLabel.classList.add('toggle-label--checked');
-  } else {
-    body.classList.remove('dark-mode');
-    toggleLabel.classList.remove('toggle-label--checked');
-  }  
-});
-
-
-//Page Scroll
-function scrollToBottom() {
+  // Page Scroll
+  function scrollToBottom() {
     const contactButton = document.querySelector('#contactButton');
     contactButton.addEventListener('click', () => {
       window.scrollTo({
@@ -128,29 +117,26 @@ function scrollToBottom() {
       });
     });
   }
-  
   scrollToBottom();
 
-// Get the button elements that are not working yet
-const aboutButton = document.querySelector("#aboutButton");
-const termsButton = document.querySelector("#termsOfService");
-const disclaimerButton = document.querySelector("#disclaimer");
-const privacyButton = document.querySelector("#privacyPolicy");
+  // Get the button elements that are not working yet
+  const aboutButton = document.querySelector("#aboutButton");
+  const termsButton = document.querySelector("#termsOfService");
+  const disclaimerButton = document.querySelector("#disclaimer");
+  const privacyButton = document.querySelector("#privacyPolicy");
 
-// Add a click event listener to the buttons
-aboutButton.addEventListener("click", () => {
-  alert("Feature not available");
-});
-termsButton.addEventListener("click", () => {
-  alert("Feature not available");
-});
-disclaimerButton.addEventListener("click", () => {
-  alert("Feature not available");
-});
-privacyButton.addEventListener("click", () => {
-  alert("Feature not available");
-});
+  // Add a click event listener to the buttons
+  aboutButton.addEventListener("click", () => {
+    alert("Feature not available");
+  });
+  termsButton.addEventListener("click", () => {
+    alert("Feature not available");
+  });
+  disclaimerButton.addEventListener("click", () => {
+    alert("Feature not available");
+  });
+  privacyButton.addEventListener("click", () => {
+    alert("Feature not available");
+  });
 
-
-}) 
-
+})
